961356b6f9229639b171c262c4cca05665c0191c
Michel Domingos <m.domingos76@gmail.com>
2017-03-29 03:19:41 +0200

    my Angular solution

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -1,4 +1,26 @@
 // MAIN CONTROLLER
 function mainController($scope) {
-    
+    $scope.firstname = '';
+    $scope.lastname = '';
+    $scope.email = '';
+
+    $scope.persons = [{
+        firstname: '',
+        lastname: '',
+        email: ''
+    }];
+
+    $scope.addPerson = function() {
+        let person = {
+            firstname: $scope.firstname,
+            lastname: $scope.lastname,
+            email: $scope.email
+
+        };
+        $scope.persons.push(person);
+        $scope.firstname = '';
+        $scope.lastname = '';
+        $scope.email = '';
+    };
+
 }
